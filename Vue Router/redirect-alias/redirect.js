fd310a4f793cceef6b8f79bc3867cdbd4323306c
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-04-17 03:10:27 +0900

    add redirect

---
+++ Vue Router/redirect-alias/redirect.js
@@ -0,0 +1,8 @@
+var router = new VueRouter({
+  routes:[
+    {path:'/a',redirect: '/b'},
+    {path:'/b',component: B},
+    {path:'/notfound',component:NotFound},
+    {path:'*',redirect:'/notfound'}
+  ]
+})
