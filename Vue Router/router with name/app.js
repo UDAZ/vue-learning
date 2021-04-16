424db57530ad5374fa55b06988d0574a7fa3c5f6
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-04-16 14:42:24 +0900

    add router with name

---
+++ Vue Router/router with name/app.js
@@ -0,0 +1,31 @@
+//ルートオプションを渡してルータインスタンスを生成します。
+var router = new VueRouter({
+  //コンポーネントをマッピングしたルート定義を配列で渡す
+  routes: [
+    {
+      path: '/top',
+      component: {
+        template: '<div>トップページです。</div>'
+      }
+    },
+    {
+      path: '/users',
+      component: {
+        template: '<div>ユーザー一覧ページです。</div>'
+      }
+    },
+    {
+      //コロンで始まるパターンマッチング
+      path: '/user/:userId',
+      name: 'user',
+      component: {
+        template: '<div>ユーザーIDは{{ $route.params.userId }}です。</div>'
+      }
+    }
+  ]
+})
+
+//ルーターのインスタンスをrootとなるVueインスタンスに渡す
+var app = new Vue({
+  router:router
+}).$mount('#app')
